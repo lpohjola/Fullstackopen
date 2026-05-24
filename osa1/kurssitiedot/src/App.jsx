e752b4b55a8794294ae2db8fd66525b6db299328
lpohjola <laura.pohjola14@gmail.com>
2026-05-24 10:42:02 +0300

    Osa 1: kurssitiedot valmis

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>Number of exercises: {totalExercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+   return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
